ecf5f1657a982a548dc273b12938d8f86924227c
Le Son <hoangsonle211@gmail.com>
2024-08-21 00:02:30 +0700

    tnp

--- labobarcodeweb/src/components/ui-kit/BarcodeScanner/BarcodeScanner.jsx
+++ labobarcodeweb/src/components/ui-kit/BarcodeScanner/BarcodeScanner.jsx
@@ -1,40 +1,39 @@
 import React, {
-    useState
-  } from "react";
-  import QrReader from "react-web-qr-reader";
-  
-  const Example = () => {
-    const delay = 500;
-  
-    const previewStyle = {
-      height: 240,
-      width: 320
-    };
-  
-    const [result, setResult] = useState("No result");
-  
-    const handleScan = (result) => {
-      if (result) {
-        alert(result);
-        // setResult(result);
-      }
-    };
-  
-    const handleError = (error) => {
-      console.log(error);
-    };
-  
-    return (
-      <>
-        <QrReader
-          delay={delay}
-          style={previewStyle}
-          onError={handleError}
-          onScan={handleScan}
-        />
-        <p>{result}</p>
-      </>
-    );
+  useState
+} from "react";
+import QrReader from "react-web-qr-reader";
+
+const Example = () => {
+  const delay = 500;
+
+  const previewStyle = {
+    height: 240,
+    width: 320
   };
-  
-  export default Example;
+
+  const [result, setResult] = useState("No result");
+
+  const handleScan = (result) => {
+    if (result) {
+      setResult(result);
+    }
+  };
+
+  const handleError = (error) => {
+    console.log(error);
+  };
+
+  return (
+    <>
+      <QrReader
+        delay={delay}
+        style={previewStyle}
+        onError={handleError}
+        onScan={handleScan}
+      />
+      <p>{result}</p>
+    </>
+  );
+};
+
+export default Example;
